5574ef76f190f92485702888289e4f64723279a3
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-10-29 17:02:23 -0400

    Add reset.css and additional functionality

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-const cdiv = document.querySelector('.grid');
+let color = "black";
+let click = true;
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  cdiv.appendChild(div);
+
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div)=> div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+  for(let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+populateBoard(16);
+
+function changeSize(input) {
+  if(input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("too many squares");
+  } 
+}
+
+function colorSquare() {
+  if(click) {
+    if(color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div)=> div.style.backgroundColor = "white");
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+  if(e.target.tagName != 'BUTTON'){
+    click = !click;
+    if(click) {
+      document.querySelector('.mode').textContent = "Mode: Coloring";
+    } else {
+      document.querySelector('.mode').textContent = "Mode: Not Coloring";
+  }
+  }
+})
