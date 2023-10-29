359493122cc7a457e4b5c649395e5068ba235118
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-10-29 18:25:43 -0400

    Finish css styling

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ populateBoard(16);
 
 function changeSize(input) {
   if(input >= 2 && input <= 100) {
+    document.querySelector(".error").style.display = "none";
     populateBoard(input);
   } else {
-    console.log("too many squares");
+    document.querySelector(".error").style.display = "flex";
   } 
 }
 
